ab3b9886c2598c69738e6f11958e08e80910b904
neotech5 <89388067+neotech5@users.noreply.github.com>
2022-06-15 21:36:32 +0800

    Create gg.sh

---
+++ gg.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo add-apt-repository ppa:neurobin/ppa
+
+wget -O /usr/bin/shco https://raw.githubusercontent.com/neotech5/encryptshell/main/setup ; chmod +x /usr/bin/shco ; mkdir /root/enkrip
+
+clear
